7d152f8366e7c416a83ee23562557b591a15d853
subepa <benny.pau@collectionhouse.com.au>
2016-08-24 14:06:25 +1000

    Reset the web page.

---
+++ GlobalHorseInfoWeb/GlobalHorseInfoWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GlobalHorseInfoWeb.Startup))]
+namespace GlobalHorseInfoWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
